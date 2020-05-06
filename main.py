e20a42028573796866dbbd35fcd2e0f1d300a937
Brian House <email@brian.house>
2020-05-05 18:37:27 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import time
+import math
+import subprocess
+import cairo
+from util import *
+
+filename = "200424_mono_.wav"
+sound = Sound(filename)
+
+# sound.plot()
+# exit()
+
+log.info("Computing spectrogram...")
+
+block_size = 512
+block_overlap = block_size / 2 # power of two, default is 128
+
+spectrum, freqs, ts, image = plt.specgram(sound.signal, NFFT=block_size, Fs=sound.rate, noverlap=block_overlap)
+
+log.info(f"--> freq bins {len(freqs)}")
+log.info(f"--> time columns {len(ts)}")
+
+log.info("Drawing...")
+
+freqs = freqs[:-90] # chop off the top
+
+width, height = 1080, 1080
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+ctx = cairo.Context(surface)
+ctx.rectangle(0, 0, width, height)
+ctx.set_source_rgba(255, 255, 255, 255)
+ctx.fill()
+ctx.stroke()
+
+THRESHOLD = .5
+for freq_n in range(len(freqs)):
+    for t_n in range(len(ts)):
+
+        v = spectrum[freq_n][t_n]
+        v = 0 if v > THRESHOLD else 255
+        r = 1.0 - (freq_n / len(freqs))
+        r *= (width/2) - 1
+        r += 1
+        a1 = (t_n / len(ts)) * 360
+        a2 = a1 + (360 / len(ts))
+        # ctx.line((x * pixel_width) / ctx.width, (y * pixel_height) / ctx.height, ((x * pixel_width) + pixel_width) / ctx.width, (y * pixel_height) / ctx.height, stroke=(v, v, v, 1.), thickness=pixel_height)
+        # ctx.arc(.5, .5, r, r, d, d+1, stroke=(0.0, 0.0, 0.0, .1), thickness=5.0)
+        ctx.set_source_rgba(v, v, v, 255)
+        ctx.arc(width/2, height/2, r, math.radians(a1), math.radians(a2))
+        ctx.stroke()
+
+
+log.info("--> done")
+ctx.stroke()
+filename = f"charts/{int(time.time())}.png"
+surface.write_to_png(filename)
+subprocess.call(["open", filename])
---
+++ util/__init__.py
@@ -0,0 +1,76 @@
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+from .log_config import log, config
+
+
+class Sound(object):
+
+    def __init__(self, path):
+        log.info("Loading sound from %s..." % path)
+        self.path = path
+        self.wf = wave.open(self.path, 'rb')
+        self.bits = self.wf.getsampwidth() * 8
+        if self.bits != 16:
+            raise NotImplementedError
+        self.signal = np.fromstring(self.wf.readframes(-1), 'Int16') # signed 16-bit samples
+        self.wf.rewind()
+        self.rate = self.wf.getframerate()
+        self.samples = len(self.signal)
+        self.duration = self.samples / self.rate
+        log.info(f"--> bits {self.bits}")
+        log.info(f"--> rate {self.rate}")
+        log.info(f"--> samples {self.samples}")
+        log.info(f"--> duration {self.duration:.2f}s")
+
+
+    def plot(self):
+
+        # 50-5000 hz
+        # need a window @ 25 hz, 0.04 seconds
+        # 0.04 / (1 / sampling_rate)
+        # at 11025, that's 441
+        # so at that sampling_rate and higher, 512 is good
+        block_size = 512
+
+        # set up plot
+        plt.rcParams['toolbar'] = 'None'
+        plt.figure(frameon=True, figsize=(15, 8), dpi=80, facecolor=(1., 1., 1.), edgecolor=(1., 1., 1.))
+
+        # show amplitude domain
+        plt.subplot(2, 1, 1)
+        plt.plot(self.signal, color=(1., 0., 0.))
+        plt.axis([0.0, self.duration * self.rate, 0-(2**self.bits/2), 2**self.bits/2]) # go to bitrate
+
+        # show spectrogram
+        plt.subplot(2, 1, 2)
+        block_overlap = block_size / 2 # power of two, default is 128
+        spectrum, freqs, ts, image = plt.specgram(self.signal, NFFT=block_size, Fs=self.rate, noverlap=block_overlap)
+        plt.axis([0.0, self.duration, 0, self.rate/2])
+
+        fig = plt.gcf()
+        fig.canvas.set_window_title(self.path)
+
+        log.info("--> freq bins %s" % len(freqs))
+        log.info("--> time columns %s" % len(ts))
+
+        plt.show()
+
+
+def normalize(signal, minimum=None, maximum=None):
+    signal = np.array(signal).astype('float')
+    if minimum is None:
+        minimum = np.min(signal)
+    if maximum is None:
+        maximum = np.max(signal)
+    signal -= minimum
+    maximum -= minimum
+    signal /= maximum
+    signal = np.clip(signal, 0.0, 1.0)
+    return signal
+
+
+def resample(ts, values, n_samples):
+    assert np.all(np.diff(ts) >= 0)
+    ts = normalize(ts)
+    return np.interp(np.linspace(0.0, 1.0, n_samples), ts, values)
