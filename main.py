9c16dce55cb0e3539b0502e6f8151d31a8f58f20
Brian House <email@brian.house>
2020-05-05 18:48:23 -0700

    coord tweaks

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import cairo
 from util import *
 
 filename = "200424_mono_.wav"
+# filename = "robin_chat_sample_11k_16_mono.wav"
 sound = Sound(filename)
 
 # sound.plot()
@@ -27,6 +28,7 @@ log.info("Drawing...")
 freqs = freqs[:-90] # chop off the top
 
 width, height = 1080, 1080
+width, height = len(freqs) * 8, len(freqs) * 8
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
 ctx = cairo.Context(surface)
 ctx.rectangle(0, 0, width, height)
@@ -41,7 +43,8 @@ for freq_n in range(len(freqs)):
         v = spectrum[freq_n][t_n]
         v = 0 if v > THRESHOLD else 255
         r = 1.0 - (freq_n / len(freqs))
-        r *= (width/2) - 1
+        # r = freq_n / len(freqs)
+        r *= ((width-50)/2) - 1
         r += 1
         a1 = (t_n / len(ts)) * 360
         a2 = a1 + (360 / len(ts))
